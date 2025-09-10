d2f36e1ea1edced64d9f138269e7046cb53f0a71
Tom Elliott <tom.w.elliott@gmail.com>
2025-09-10 16:45:49 -0400

    Update tag and revision for formula

--- Formula/fromsource.rb
+++ Formula/fromsource.rb
@@ -1,8 +1,8 @@
 class Fromsource < Formula
   desc "An example installing a Go app from source"
   url "https://github.com/theothertomelliott/homebrew-example.git",
-      tag:      "v0.3.11",
-      revision: "3decca51762304c12ab9eae7f290cf9a0416f859"
+      tag:      "v0.0.2",
+      revision: "0b2cebbda8eb92c977b246c19e8bd21e5226c752"
   license "MIT"
   head "https://github.com/theothertomelliott/homebrew-example.git", branch: "main"
 
