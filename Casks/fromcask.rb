8fec48436c89ac25fad90fdcb4b5a2f7b3069108
Tom Elliott <tom.w.elliott@gmail.com>
2025-09-10 16:54:14 -0400

    Change binary name for fromcask

--- Casks/fromcask.rb
+++ Casks/fromcask.rb
@@ -8,12 +8,12 @@ cask "fromcask" do
   on_intel do
     url "https://github.com/theothertomelliott/homebrew-example/releases/download/v#{version}/homebrew-example-darwin-amd64.tar.gz"
     sha256 "b6a53e31c9589af4652c83d2e30b466dd7056c50bd50302fa086408c2d3b78d2"
-    binary "homebrew-example-darwin-amd64/homebrew-example", target: "homebrew-example"
+    binary "homebrew-example-darwin-amd64/homebrew-example", target: "fromcask"
   end
 
   on_arm do
     url "https://github.com/theothertomelliott/homebrew-example/releases/download/v#{version}/homebrew-example-darwin-arm64.tar.gz"
     sha256 "8ef6aba23c7875cc9f9a9fdee859d3e9c379283cb79677f583cf9f6d595c68fb"
-    binary "homebrew-example-darwin-arm64/homebrew-example", target: "homebrew-example"
+    binary "homebrew-example-darwin-arm64/homebrew-example", target: "fromcask"
   end
 end
